ba840f5594bf7529d8c05170c34c2e789f65eda2
yiwwhl <yiwwhl@qq.com>
2022-11-20 20:08:42 +0800

    feat: add plugin nvim-treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,6 +20,8 @@ packer.startup({
     use("glepnir/dashboard-nvim")
     -- project
     use("ahmedkhalf/project.nvim")
+    -- treesitter
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
   end,
   config = {
     display = {
